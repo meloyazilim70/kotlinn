da5a7268ff518dbc4407748c34c646927656ba27
Melike Ay <melike.ay.122@gmail.com>
2022-10-11 20:49:28 +0300

    finish

--- src/main/kotlin/1 Getting Started/HelloWorld.kt
+++ src/main/kotlin/1 Getting Started/HelloWorld.kt
@@ -1,10 +1,13 @@
 package `1 Getting Started`
 
+import kotlin.math.pow
+
 fun main() {
     // value inmutable
     // variable mutable
     val name: String = "Gökhan"
     var SurName: String = "ÖZTÜRK"
+    3.14.pow(2.1)
     println(name)
     println(SurName)
 
@@ -12,5 +15,17 @@ fun main() {
  //   name = "Ahmet"
     println(name)
     println(SurName)
-
 }
+fun main(args: Array<String>) {
+    println("Hello World")
+    print("Melike")
+    print("Ay")
+    println("Melike")
+    print("Ay")
+}
+// Tekil yorum satırı
+/* Çoklu yorum Satırı*/
+/**
+ * Dökümantasyon yorum satırı
+ */
+// Todo (Burası daha sonra düzeltilecek)
