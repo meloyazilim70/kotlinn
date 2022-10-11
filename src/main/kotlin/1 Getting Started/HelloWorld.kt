7a40adaeac5cba13e50c05c55d3c5eb862f2bf8f
Melike Ay <melike.ay.122@gmail.com>
2022-10-11 20:26:06 +0300

    begining

---
+++ src/main/kotlin/1 Getting Started/HelloWorld.kt
@@ -0,0 +1,16 @@
+package `1 Getting Started`
+
+fun main() {
+    // value inmutable
+    // variable mutable
+    val name: String = "Gökhan"
+    var SurName: String = "ÖZTÜRK"
+    println(name)
+    println(SurName)
+
+    SurName = "Genç"
+ //   name = "Ahmet"
+    println(name)
+    println(SurName)
+
+}
