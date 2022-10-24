8e8ce4b3d28ffc7803c50042f40fe88f541b6c0f
Melike Ay <melike.ay.122@gmail.com>
2022-10-24 17:28:46 +0300

    Conversion

--- src/main/kotlin/2 basics/2 Type Conversion.kt
+++ src/main/kotlin/2 basics/2 Type Conversion.kt
@@ -1,2 +1,17 @@
 package `1 Getting Started`.`2 basics`
 
+fun main() {
+    val schoolNumber =126.toByte()
+    val convertedvalue: Short = schoolNumber.toShort()
+    println("schoolNumber :"+schoolNumber)
+    println("convertedValue :"+ convertedvalue)
+
+    val tcIdentityNumber = 15455789645
+    val convertedInt = tcIdentityNumber.toInt()
+    println("converted :"+convertedInt)
+
+    val totalValue: Long = tcIdentityNumber+schoolNumber //Long + Byte = Long
+    val sameValue = 127+127 // Byte degil, Short degil, int olacaktir. sameValue=254
+    val byte: String = "5"
+    val IntValue =Integer.parseInt(byte)
+}
